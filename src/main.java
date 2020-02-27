bbaaf01bbfb317a203951812e8a912e2debdb27e
LCAndrioli <luiz.c.andrioli@gmail.com>
2020-02-27 11:58:25 -0300

    commit inicial

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+
+    public static void metodo(int x){
+        int y = 0;
+        y = x + 1;
+        System.out.println(y);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("ZAPZAP");
+        metodo(10);
+    }
+
+}
